16859ab3b52a6b057387e48910b448ca69a3f7bd
willsinn <30275717+willsinn@users.noreply.github.com>
2018-06-13 15:49:51 -0400

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Todo-header">
+       <h1> React Todos </h1>
+      </div>
+
+    );
+  }
+}
+
+export default Header;
---
+++ src/Components/InputTodos.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class InputTodos extends Component {
+
+
+  render() {
+    return (
+      <div>
+      
+      </div>
+
+    );
+  }
+}
+
+export default InputTodos;
